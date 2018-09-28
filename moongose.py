c212e0526c5992c88bcd970f0d90c79ac0c0a678
Alejandro Joya <joya.a.cruz@gmail.com>
2018-09-28 03:27:27 -0500

    add login endpoint
    Signed-off-by: Alejandro Joya <joya.a.cruz@gmail.com>

--- moongose.py
+++ moongose.py
@@ -12,22 +12,35 @@ class Mongoose(object):
         }
         response = self.baseclient.request(method,resource_path,json=data)
 
-
-        print (response)
-
+        self.baseclient.token = response["data"]["token"]
+    def login(self,username,password):
+        method = "POST"
+        resource_path = "/login"
+        data = {
+            "username":username,
+            "password":password
+        }
+        response = self.baseclient.request(method,resource_path,json=data)
+        print(response)
+        self.baseclient.token = response["data"]["token"]
 
 class _Baseclient(object):
     def __init__(self, host,port):
         self.host = "http://{}:{}/api".format(host,port)
+        self.token = None
+
     def request(self,method,resource_path,**kwargs):
         path = self.host + resource_path
+        if self.token:
+            kwargs["headers"]={"auth":self.token}
 
         response = requests.request(method,path,**kwargs)
         
-        if 200 < response.status_code < 210:
+        if 200 <= response.status_code < 210:
             return response.json()
         elif 400 <= response.status_code < 410:
             raise ValueError(response.json()["message"])
         else:
             response.raise_for_status()
+
     
